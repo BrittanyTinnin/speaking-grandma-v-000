dee0772d149289d6249ef3f745e77d32a4846f0b
Brittany Tinnin <bntinnin7@gmail.com>
2018-05-25 23:44:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase?
+    return "HUH?! SPEAK UP, SONNY!"
   end
   # if phrase
   #   puts "HUH?! SPEAK UP, SONNY!"
